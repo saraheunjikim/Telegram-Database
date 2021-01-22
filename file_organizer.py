c3a0d554871abbb5365adfd52b57a53fa1f75016
Sarah <saraheunjikim@gmail.com>
2021-01-21 20:37:16 -0600

    Update file_organizer.py
    There is permission error needs to be fixed, but I'm working on it!!

--- file_organizer.py
+++ file_organizer.py
@@ -1,13 +1,37 @@
+import os
 from os import listdir
+import shutil
 
-my_path = r'C:\Users\Muffin\Desktop\python\projects\Telegram-Database'
+source_dir = 'C:/Users/Muffin/Desktop/python/projects/Telegram-Database/'
+destination_dir = 'C:/Users/Muffin/Desktop/python/projects/Telegram-Database/files/'
+files = [file for file in listdir(source_dir) if file.endswith('csv')]
 
 
 # Get all file names in the directory
 def file_names():
-    files = [file for file in listdir(my_path) if file.endswith('csv')]
+    files = [file for file in listdir(source_dir) if file.endswith('csv')]
     return files
 
-files = [file for file in listdir(my_path) if file.endswith('csv')]
-print(files)
-# Open and read files
+
+# Read files and separate them
+def categorize_files(files):
+    for file in files:
+        with open(file, mode='r', encoding='utf8') as f:
+            text = f.readline().split(",")
+            if 'Job ID' in text[0]:
+                move_files(file, 'jobs/')
+                print("This is a job related file")
+            else:
+                print("no")
+
+    return True
+
+
+# Move files
+def move_files(file_name, category):
+    print(file_name)
+    print(category)
+    return shutil.move(source_dir + file_name, destination_dir + category + file_name)
+
+
+categorize_files(files)
