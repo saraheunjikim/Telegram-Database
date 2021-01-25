6c2832f592669907f73167996715a77c545af235
Sarah <saraheunjikim@gmail.com>
2021-01-25 17:39:08 -0600

    Upload to S3 Bucket
    Upload the files to AWS S3 bucket using boto

---
+++ run_script.py
@@ -0,0 +1,12 @@
+import telegram_get
+import file_organizer
+
+# 1. run telegram_get.py to get files
+
+# 2. run file_organizer.py to sort files
+
+# 3. Upload the files to S3 bucket
+
+# 4. Process the files using AWS Lambda
+
+# 5. Move the files to AWS RedShift
---
+++ upload_to_s3.py
@@ -0,0 +1,24 @@
+import token_id
+import boto3
+import os
+
+client = boto3.client('s3',
+                      aws_access_key_id=token_id.access_key,
+                      aws_secret_access_key=token_id.secret_access_key)
+
+
+for folder in os.listdir('C:/Users/Muffin/Desktop/python/projects/Telegram-Database/files'):
+    path = 'C:/Users/Muffin/Desktop/python/projects/Telegram-Database/files'
+    upload_file_bucket = 'telegram-data-saraheunjikim'
+
+    if folder == 'corona':
+        for file in os.listdir('C:/Users/Muffin/Desktop/python/projects/Telegram-Database/files/corona'):
+            upload_file_key_txt = 'corona/' + str(file)
+            client.upload_file(f'{path}/corona/{file}', upload_file_bucket, upload_file_key_txt)
+            print(file + " uploaded")
+
+    elif folder == 'jobs':
+        for file in os.listdir('C:/Users/Muffin/Desktop/python/projects/Telegram-Database/files/jobs'):
+            upload_file_key_txt = 'jobs/' + str(file)
+            client.upload_file(f'{path}/jobs/{file}', upload_file_bucket, upload_file_key_txt)
+            print(file + " uploaded")
